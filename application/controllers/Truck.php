589ac879df330b77c6a3719cc27f5d8d418366a6
dev_ragrin <kwanterpepen@gmail.com>
2019-07-12 14:38:01 +0800

    perubahan variabel dari city ke truck

--- application/controllers/Truck.php
+++ application/controllers/Truck.php
@@ -3,35 +3,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Truck extends MY_Controller{
     public function index(){
-        $this->navmenu('Input Data Kota','add/vw_input_data_city','','','');
+        $this->navmenu('Input Data Truk','add/vw_input_data_truk','','','');
     }
 
     public function edit($id){
         $data['city'] = $this->city->getData($id);
-        $this->navmenu('Edit Data Kota','edit/vw_edit_data_city','','',$data);
+        $this->navmenu('Edit Data Kota','edit/vw_edit_data_truk','','',$data);
     }
 
     public function ajax_list(){
-        $list = $this->city->get_datatable();
+        $list = $this->truck->get_datatable();
         $data = array();
         $no = $_POST['start'];
 
-        foreach ($list as $city) {
+        foreach ($list as $r) {
             $no++;
             $row = array();
             $row[] = '<center style="font-size: small">'.$no;
-            $row[] = '<center style="font-size: small">'.$city->city_code;
-            $row[] = '<center style="font-size: small">'.$city->city_name;
-            $row[] = '<center><a href="javascript:void(0)" title="Edit" onclick="edit('."'".$city->idm_city."'".')"><i class="material-icons">launch</i></a>
-                              <a href="javascript:void(0)" title="Hapus" onclick="del('."'".$city->idm_city."'".')"><i class="material-icons">delete_forever</i></a>';
+            $row[] = '<center style="font-size: small">'.$r->city_code;
+            $row[] = '<center style="font-size: small">'.$r->city_name;
+            $row[] = '<center><a href="javascript:void(0)" title="Edit" onclick="edit('."'".$r->idm_truck."'".')"><i class="material-icons">launch</i></a>
+                              <a href="javascript:void(0)" title="Hapus" onclick="del('."'".$r->idm_truck."'".')"><i class="material-icons">delete_forever</i></a>';
 
             $data[] = $row;
         }
 
         $output = array(
             "draw" => $_POST['draw'],
-            "recordsTotal" => $this->city->countAll(),
-            "recordsFiltered" => $this->city->countFiltered(),
+            "recordsTotal" => $this->truck->countAll(),
+            "recordsFiltered" => $this->truck->countFiltered(),
             "data" => $data,
         );
         //output to json format
@@ -39,12 +39,12 @@ class Truck extends MY_Controller{
     }
 
     public function delete($id){
-        $this->city->deleteData($id);
+        $this->truck->deleteData($id);
         echo json_encode(array("status" => TRUE));
     }
 
     public function addData() {
-        $result = $this->city->saveData($_POST);
+        $result = $this->truck->saveData($_POST);
 
         if ($result)
             $this->session->set_flashdata('notif', '<div class="alert alert-success" role="alert"> 
@@ -65,8 +65,8 @@ class Truck extends MY_Controller{
     }
 
     public function updateData() {
-        $id = $this->input->post('idm_city');
-        $result = $this->city->updateData($_POST);
+        $id = $this->input->post('idm');
+        $result = $this->truck->updateData($_POST);
 
         if ($result)
             $this->session->set_flashdata('notif', '<div class="alert alert-success" role="alert"> 
@@ -87,7 +87,7 @@ class Truck extends MY_Controller{
     }
 
     public function get_data($id){
-        $result = $this->city->get_data($id);
+        $result = $this->truck->get_data($id);
         echo json_encode($result);
     }
 }
