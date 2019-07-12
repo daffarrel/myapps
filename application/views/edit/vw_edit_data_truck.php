4924e324c3ff8d93c4eeb2088c099fd868919be8
dev_ragrin <kwanterpepen@gmail.com>
2019-07-12 14:55:07 +0800

    inisialisasi file edit data truk

---
+++ application/views/edit/vw_edit_data_truck.php
@@ -0,0 +1,77 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+<section class="content">
+    <div class="container-fluid">
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="card">
+                    <div class="header">
+                        <h2>Edit Data Truk</h2>
+                    </div>
+                    <div class="body">
+                        <?php echo $this->session->flashdata('notif');?>
+                        <form id="form_input" action="<?php echo base_url('truck/updateData')?>" method="POST" enctype="multipart/form-data">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="city_code" class="form-label">Kode Kota</label>
+                                    <input hidden value="<?php echo $attr['city']['idm_city']?>" id="idm_city" name="idm_city">
+                                    <div class="input-group">
+                                        <span class="input-group-addon">
+                                            <i class="material-icons">perm_identity</i>
+                                        </span>
+                                        <div class="form-line">
+                                            <input type="text" id="city_code" name="city_code" required="required" class="form-control" value="<?php echo $attr['city']['city_code']?>">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="city_name" class="form-label">Nama Bank</label>
+                                    <div class="input-group">
+                                        <span class="input-group-addon">
+                                            <i class="material-icons">keyboard</i>
+                                        </span>
+                                        <div class="form-line">
+                                            <input id="city_name" name="city_name" value="<?php echo $attr['city']['city_name']?>" required="required" class="form-control" type="text">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                            <button class="btn bg-red waves-effect" onclick="cancel();" type="button"><i class="material-icons">undo</i><span>Cancel</span></button>
+                            <button class="btn bg-blue waves-effect" type="reset"><i class="material-icons">clear</i><span>Reset</span></button>
+                            <button type="submit" class="btn bg-orange waves-effect"><i class="material-icons">save</i><span>Simpan</span></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script type="text/javascript">
+    function cancel() {
+        window.location.replace('<?php echo site_url('master/page/city')?>')
+    }
+
+    function master() {
+        window.location.replace('<?php echo site_url('master/page/city')?>')
+    }
+
+    $(function () {
+        var form = $('#form_input');
+        form.validate({
+            highlight: function (input) {
+                $(input).parents('.form-line').addClass('error');
+            },
+            unhighlight: function (input) {
+                $(input).parents('.form-line').removeClass('error');
+            },
+            errorPlacement: function (error, element) {
+                $(element).parents('.form-label').append(error);
+            }
+        });
+    });
+</script>
