e5bcc2fa3a9a895b917ae7061962f31923307028
dev_armstrong <kwanterpepen@gmail.com>
2019-09-24 23:35:34 +0800

    Perbaikan tampilan ubah data driver

--- application/views/edit/vw_edit_data_driver.php
+++ application/views/edit/vw_edit_data_driver.php
@@ -2,104 +2,76 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 ?>
-<section class="content">
+<section class="content-wrapper">
     <div class="container-fluid">
-        <div class="row clearfix">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="card">
-                    <div class="header">
-                        <h2>Ubah Data Supir</h2>
-                    </div>
-                    <div class="body">
-                        <?php echo $this->session->flashdata('notif');?>
-                        <form id="form_input" action="<?php echo base_url('driver/updateData')?>" method="POST" enctype="multipart/form-data">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="agent_name" class="form-label">Nama Supir</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon">
-                                            <i class="material-icons">perm_identity</i>
-                                        </span>
-                                        <div class="form-line">
-                                            <input value="<?php echo $attr['data']->idm_driver?>" hidden id="idm" name="idm">
-                                            <input value="<?php echo $attr['data']->driver_name?>" required id="nama_supir" name="nama_supir" class="form-control" type="text">
-                                        </div>
+        <section class="content-header">
+            <h1>Tambah Data Driver</h1>
+            <ol class="breadcrumb">
+                <li><a href="<?php echo base_url()?>"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li><a href="#"> Master Data</a></li>
+                <li class="active"><a href="#"> Tambah Data Driver</a></li>
+            </ol>
+        </section>
+        <section class="content">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-12">
+                    <div class="box box-default">
+                        <div class="box-body">
+                            <?php echo $this->session->flashdata('notif');?>
+                            <form id="form_input" action="<?php echo base_url('driver/updateData')?>" method="POST" enctype="multipart/form-data">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="agent_name" class="form-label">Nama Supir</label>
+                                        <input value="<?php echo $attr['data']->idm_driver?>" hidden id="idm" name="idm">
+                                        <input value="<?php echo $attr['data']->driver_name?>" required id="nama_supir" name="nama_supir" class="form-control" type="text">
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="agent_name" class="form-label">No Sim</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon">
-                                            <i class="material-icons">format_list_numbered</i>
-                                        </span>
-                                        <div class="form-line">
-                                            <input value="<?php echo $attr['data']->license_number?>" required id="no_sim" name="no_sim" class="form-control" type="text">
-                                        </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="agent_name" class="form-label">No Sim</label>
+                                        <input value="<?php echo $attr['data']->license_number?>" required id="no_sim" name="no_sim" class="form-control" type="text">
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label for="agent_name" class="form-label">Tempat Lahir</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon">
-                                            <i class="material-icons">place</i>
-                                        </span>
-                                        <div class="form-line">
-                                            <input value="<?php echo $attr['data']->dob_city?>" required id="tmpt_lahir" name="tmpt_lahir" class="form-control" type="text">
-                                        </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="agent_name" class="form-label">Tempat Lahir</label>
+                                        <input value="<?php echo $attr['data']->dob_city?>" required id="tmpt_lahir" name="tmpt_lahir" class="form-control" type="text">
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label for="agent_name" class="form-label">Tanggal Lahir</label>
-                                    <div class="input-group">
-                                        <div class="form-line">
-                                            <input value="<?php echo $attr['data']->dob?>" required id="tgl_lahir" name="tgl_lahir" class="form-control datepicker" type="text">
-                                        </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="agent_name" class="form-label">Tanggal Lahir</label>
+                                        <input value="<?php echo $attr['data']->dob?>" required id="tgl_lahir" name="tgl_lahir" class="form-control datepicker" type="text">
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label for="agent_name" class="form-label">Alamat</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon">
-                                            <i class="material-icons">airport_shuttle</i>
-                                        </span>
-                                        <div class="form-line">
-                                            <input value="<?php echo $attr['data']->address?>" required id="alamat" name="alamat" class="form-control" type="text">
-                                        </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="agent_name" class="form-label">Alamat</label>
+                                        <input value="<?php echo $attr['data']->address?>" required id="alamat" name="alamat" class="form-control" type="text">
                                     </div>
                                 </div>
-                            </div>
-                            <br>
-                            <button class="btn bg-red waves-effect" onclick="cancel();" type="button"><i class="material-icons">undo</i><span>Cancel</span></button>
-                            <button class="btn bg-blue waves-effect" type="reset"><i class="material-icons">clear</i><span>Reset</span></button>
-                            <button type="submit" class="btn bg-orange waves-effect"><i class="material-icons">save</i><span>Simpan</span></button>
+                                <div class="col-md-12">
+                                    <br>
+                                    <button class="btn btn-danger" onclick="cancel();" type="button"><span>Cancel</span></button>
+                                    <button class="btn btn-warning" type="reset"><span>Reset</span></button>
+                                    <button type="submit" class="btn btn-primary"><span>Simpan</span></button>
+                                </div>
                         </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     </div>
 </section>
 <script type="text/javascript">
     function cancel() {
-        window.location.replace('<?php echo site_url('master/page/driver')?>')
+        window.location.replace('<?php echo site_url('main/master/driver')?>')
     }
 
-    function master() {
-        window.location.replace('<?php echo site_url('master/page/driver')?>')
-    }
-
-    $('.datepicker').bootstrapMaterialDatePicker({
-        format: 'YYYY-MM-DD',
-        clearButton: true,
-        weekStart: 1,
-        time: false
+    $('.datepicker').datepicker({
+        format     : 'yyyy-mm-dd',
+        clearBtn   : true,
+        autoclose  : true,
     });
 </script>
